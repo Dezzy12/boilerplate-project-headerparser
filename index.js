4426554c13743037164f49fbcb2d94a2e38797cb
Dezzy12 <desmondneba134@gmail.com>
2023-08-05 13:55:27 +0100

    Modified index.js file for the freecodecamp challenge

--- index.js
+++ index.js
@@ -19,12 +19,23 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get('/api/whoami', function (req, res) {
+  const ip = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress: ip, language: language, software: software });
+});
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3050, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
